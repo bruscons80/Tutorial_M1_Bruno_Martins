6257746b0a0ea725f030bf34d556fd3088978265
bruscons80 <126347307+bruscons80@users.noreply.github.com>
2023-03-16 01:04:15 -0300

    correcao do codigo semana 5
    ponderada semana 5 versao corrigida

--- SEMANA_05/02_ATIVIDADES/correecao codigo/Node2D.gd
+++ SEMANA_05/02_ATIVIDADES/correecao codigo/Node2D.gd
@@ -2,19 +2,19 @@ extends Node2D
 
 var teste = false
 var nome = "" #nome tem que ser instanciado como variavel.
-var numero = 0 #variavel tem que ter o nome de numero, e assim, ser corrijida em suas respectivas chamadas.
+var Numero = 0 #variavel tem que ter o nome de numero, e assim, ser corrijida em suas respectivas chamadas.
 var lista = [] #Lista tem que ser declarada como variavel.
 
 func _on_Button_pressed():
-	numero = int($LineEdit.text) #Faltou o cifrao antes do LineEdit
+	Numero = int($LineEdit.text) #Faltou o cifrao antes do LineEdit
 	nome = $LineEdit.text #nome e o input, coloquei ele primeiro para declarar corretamente.
 
 func _on_Button2_pressed():
-	var Numero = numero #Declarei Numero = numero para nao ter que mudar muita coisa e mesmo assim ficar correto.
+	var NumeroAux = Numero #Declarei Numero = numero para nao ter que mudar muita coisa e mesmo assim ficar correto.
 	for i in range(10):
 		Numero += i
-	lista.append(Numero)
-	$Label.text = str(Numero) #Numero convertido em String para ser parte do LabelText
+	lista.append(NumeroAux)
+	$Label.text = str(NumeroAux) #Numero convertido em String para ser parte do LabelText
 
 func _on_Button3_pressed():
 # Mudando o nome do usuário de acordo com os dados da lista
