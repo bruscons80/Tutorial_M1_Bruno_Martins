e8fd5777cdaafb9c758499153f31a72bf1952bad
bruscons80 <126347307+bruscons80@users.noreply.github.com>
2023-03-16 09:57:47 -0300

    ponderada versao corrigida semana 3
    ponderada da semana 3 corrigida

--- SEMANA_03/02_ATIVIDADES/ponderada/Node2D.gd
+++ SEMANA_03/02_ATIVIDADES/ponderada/Node2D.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
-var dado1
 var dado2
 var dado3
 var resultado
+var numero
 
-func _on_Button_pressed():
-	dado1 = float($dado1.text)
-	dado2 = float($dado2.text)
-	dado3 = float($dado3.text)
-	resultado = dado1 + dado2 + dado3
+func _on_Button1_pressed():
+	$ColorRect2/resposta2.text = "1, 2, 3, 4, 5"
 	
-	$ColorRect/resposta.text = "SUA SOMA: "+ str(resultado)
+func _on_Button2_pressed():
+	dado2 = float($dado2.text)
+	resultado = dado2 
+	$ColorRect/resposta.text = "Seu Texto: "+ str(resultado)
+
+func _on_Button3_pressed():
+	dado3 = $dado3.text
+	resultado = "O texto digitado foi: " + dado3
+	$ColorRect3/resposta3.text = ""+ str(resultado)
+
